a6c315bbcfa75e2567f7de4c02dcaa5dcaf1056b
Kevin San Gabriel <kevinsangabriel@Kevins-MacBook-Pro-2.local>
2020-12-06 01:13:58 -0800

    added keypress events

--- src/main/java/Gameplay.java
+++ src/main/java/Gameplay.java
@@ -2,6 +2,7 @@ package src.main.java;
 
 import java.awt.event.*;
 import java.awt.Graphics;
+import java.awt.Color;
 import javax.swing.JPanel;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -11,27 +12,47 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
     private Boolean play = false;
     private int numBricks = 21;
     private int score = 0;
+
     private int playerPosX = 310;
-
-    private int delay = 8;
-
-    
+    private int ballPosX = 120;
+    private int ballPosY = 350;
+    private int ballDirX = -1;
+    private int ballDirY = -2;
 
 
     public Gameplay() {
         addKeyListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
+        
     }
 
     public void paint(Graphics g) {
+
+        // Paint background
+        g.setColor(Color.black);
+        g.fillRect(1, 1, 692, 592);
+
+        // Paint borders
+        g.setColor(Color.yellow);
+        g.fillRect(0, 0, 3, 592);
+        g.fillRect(0, 0, 692, 3);
+        g.fillRect(691, 0, 3, 592);
+
+        // Paint player
+        g.setColor(Color.green);
+        g.fillRect(playerPosX, 550, 100, 8);
+
+        // Paint ball
+        g.setColor(Color.yellow);
+        g.fillRect(ballPosX, ballPosY, 20, 20);
 
     }
     
     @Override
     public void actionPerformed(ActionEvent e) {
         // TODO Auto-generated method stub
-
+        repaint();
     }
 
     @Override
@@ -43,13 +64,35 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
     @Override
     public void keyPressed(KeyEvent e) {
         // TODO Auto-generated method stub
-
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            if (playerPosX >= 600) {
+                playerPosX = 600;
+            } else {
+                moveRight();
+            }
+        }
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            if (playerPosX <= 10) {
+                playerPosX = 0;
+            } else {
+                moveLeft();
+            }
+        }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
         // TODO Auto-generated method stub
 
+    }
+
+    public void moveRight() {
+        play = true;
+        playerPosX += 20;
+    }
+    public void moveLeft() {
+        play = true;
+        playerPosX -= 20;
     }
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,19 @@
 package src.main.java;
 
 import javax.swing.JFrame;
-import javax.swing.WindowConstants; 
+import javax.swing.WindowConstants;
+import java.util.logging.Logger;
+
+
 
 public class Main { 
+
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
     
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         Gameplay gamePlay = new Gameplay();
-        frame.setBounds(10,10,700, 600);
+        frame.setBounds(10, 10, 700, 600);
         frame.setTitle("Brick Breaker");
         frame.setResizable(false);
         frame.setVisible(true);
